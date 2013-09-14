e7afc2af5a0f415be0d7f1f9d23be101049cb725
Mario Steinhoff <mario.steinhoff@catapullt.de>
2013-09-14 23:30:51 +0200

    link local bash repository to dotfiles folder
    Add symlinks for bash_history and bash_library
    Add symlink support to install-files.sh

--- install-files.sh
+++ install-files.sh
@@ -6,15 +6,34 @@ DB=~/.dotbackup
 echo ""
 echo "Installing dotfiles to $HOME"
 
+function getlink
+{
+  echo `python -c 'import os,sys;print os.path.realpath(sys.argv[1])' $1`
+}
+
 mkdir $DB
 for source in `ls "$DF/files/"`;
 do
   dotfile="$HOME/.$source"
 
   needlink="yes"
-  if [ -e "$dotfile" ]
+  if [ -f "$dotfile" ] || [ -h "$dotfile" ]
   then
-    diff "$DF/files/$source" "$dotfile"
+    if [ ! -h $DF/files/$source ];
+    then
+      diff "$DF/files/$source" "$dotfile"
+      SAME=$?
+    else
+      AL=`getlink $DF/files/$source`
+      BL=`getlink $dotfile`
+
+      if [  "$AL" = "$BL" ];
+      then
+        SAME=0
+      else
+        SAME=1
+      fi
+    fi
 
     if [ "$?" -eq "0" ]
     then
@@ -28,8 +47,16 @@ do
 
   if [ "x$needlink" == "xyes" ]
   then
-    echo "-> Installing $DF/files/$source to $dotfile"
-    ln -s -f "$DF/files/$source" "$dotfile"
+    if [ ! -h $DF/files/$source ];
+    then
+      echo "-> Installing $DF/files/$source to $dotfile"
+      ln -s -f $DF/files/$source $dotfile
+    else
+      RP=$(getlink $DF/files/$source)
+      echo "-> Installing $RP to $dotfile"
+      ln -s -f $RP "$dotfile"
+    fi
+    echo ""
   fi
 done
 
